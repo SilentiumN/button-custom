5dca9d1ff8419ec0a21ce04ed526bb4e360fef37
Anastasiia Morozova <naskalelk@gmail.com>
2022-12-28 07:26:22 +0400

    remove unusable setting

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,7 @@ import vue from '@vitejs/plugin-vue'
 const path = require("path");
 
 export default defineConfig({
-  plugins: [vue({
-    template: {
-      transformAssetUrls: {
-          includeAbsolute: false,
-      },
-  },
-  })],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
