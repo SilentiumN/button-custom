9cf7520af8e7914fec1c83530f3c1fba83879d3d
Anastasiia Morozova <naskalelk@gmail.com>
2022-12-28 04:55:53 +0400

    Add buttons examples

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { h } from "vue";
+import ButtonCustom from "@/components/ButtonCustom";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    render() {
+        return h("div", [
+            h("div", [h(ButtonCustom, { href: "#", text: "Напомнить PIN" })]),
+            h("div", [
+                h(ButtonCustom, { icon: "arrow", theme: "primary" }),
+                h(ButtonCustom, { icon: "exit", theme: "danger" }),
+                h(ButtonCustom, { icon: "quest", theme: "question" }),
+                h(ButtonCustom, { icon: "chevron", theme: "primary" }),
+                h(ButtonCustom, { icon: "google", theme: "danger" }),
+                h(ButtonCustom, { icon: "vk", theme: "info" }),
+                h(ButtonCustom, { icon: "class", theme: "action" }),
+                h(ButtonCustom, { icon: "pen", theme: "primary" }),
+            ]),
+            h("div", [
+                h(ButtonCustom, {
+                    timer: 180,
+                    text: "повторное письмо",
+                    disabled: true,
+                }),
+            ]),
+            h("div", [
+                h(ButtonCustom, { theme: "primary", text: "Отправить письмо" }),
+                h(ButtonCustom, {
+                    theme: "secondary",
+                    text: "Отправить письмо",
+                }),
+                h(ButtonCustom, { theme: "warning", text: "Отправить письмо" }),
+                h(ButtonCustom, { text: "Отправить письмо", disabled: true }),
+                h(ButtonCustom, { theme: "info", text: "Отправить письмо" }),
+                h(ButtonCustom, { theme: "danger", text: "Отправить письмо" }),
+                h(ButtonCustom, { theme: "action", text: "Отправить письмо" }),
+                h(ButtonCustom, {
+                    theme: "info",
+                    text: "Отправить письмо",
+                    timer: 20,
+                    icon: "chevron",
+                }),
+            ]),
+        ]);
+    },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+body {
+    background: #1e1e1e;
 }
 </style>
